7914edcc92b37e990109388a15196fdcea8da3c8
dimon-mirchuk <dimon.mirchuk@gmail.com>
2021-10-25 20:44:34 +0300

    Поднял React

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      {/* <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={statisticalData} />;
+      <Statistics stats={statisticalData} /> */}
+    </>
+  );
+}
+
+export default App;
